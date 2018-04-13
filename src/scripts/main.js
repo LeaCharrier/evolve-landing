729632c38858e92fb16cbbaf6702de7b77ad619b
Léa Charrier <leacharrier@MacBook-Pro-de-Lea.local>
2018-04-13 10:17:34 +0200

    add jquery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
-console.log('jquery ok');
+jQuery(document).ready(function($) {
+  scrollNav();
+});
+
+
+  var scrollNav = function() {
+    $(window).scroll(function() {    
+      var scroll = $(window).scrollTop();    
+      if (scroll > 100) {
+        console.log('scroll');
+      }
+    });
+  }
